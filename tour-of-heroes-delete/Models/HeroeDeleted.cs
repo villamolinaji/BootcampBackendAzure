524d115a88a69bf2bdbe90d3284872622feeeb26
villamolinaji <85756710+villamolinaji@users.noreply.github.com>
2021-12-11 11:52:44 +0100

    Add files via upload

---
+++ tour-of-heroes-delete/Models/HeroeDeleted.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tour_of_heroes_delete.Models
+{
+    public class HeroeDeleted
+    {
+        public string HeroeName { get; set; }
+        public string AlterEgoName { get; set; }
+    }
+}
